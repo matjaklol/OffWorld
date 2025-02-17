758efa05029b4fc357116a42af09d9852dcac014
matjaklol <lolmatjak@gmail.com>
2025-02-16 21:03:54 -0700

    Revert player movement speed changes.

--- Offworld/src/game/entities/Player.java
+++ Offworld/src/game/entities/Player.java
@@ -12,7 +12,7 @@ public class Player extends Entity{
 		super(world);
 		this.keyboard = world.keyboard;
 		this.vec = new MVector(world);
-		this.vec.maxSpeed(30000f);
+		this.vec.maxSpeed(300f);
 		this.vec.mass(63);
 //		this.vec.maxAcceleration(50f);
 		this.vec.setFrictionConstant(0.5f);
